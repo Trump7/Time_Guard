448a36efbc1e98aa238439f2f487f12257de8d0f
Trump7 <ma332651@ucf.edu>
2024-08-07 15:51:52 -0400

    Updated some backend features
    Added ability to add, edit, and delete employees.  Also can now get employees data

--- timecard-system/routes/users.js
+++ timecard-system/routes/users.js
@@ -36,4 +36,49 @@ router.post('/validate', async(req, res) => {
     }
 });
 
+// Edit user
+router.put('/:id', async(req, res) => {
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!user) {
+            return res.status(404).send('User not found');
+        }
+        res.send(user);
+        console.log(`User ${user.name} has been updated!`);
+    } catch (error) {
+        console.log('User could not be updated');
+        res.status(400).send(error);
+    }
+});
+
+// Delete user
+router.delete('/:id', async(req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        if (!user) {
+            return res.status(404).send('User not found');
+        }
+        res.send(user);
+        console.log(`User ${user.name} has been deleted!`);
+    } catch (error) {
+        console.log('User could not be deleted');
+        res.status(400).send(error);
+    }
+});
+
+// Fetch user by ID
+router.get('/:id', async(req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        if (!user) {
+            return res.status(404).send('User not found');
+        }
+        res.send(user);
+        console.log(`User ${user.name} has been fetched!`);
+    } catch (error) {
+        console.log('User could not be fetched');
+        res.status(400).send(error);
+    }
+});
+
 module.exports = router;
