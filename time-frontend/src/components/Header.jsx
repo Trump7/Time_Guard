7946c6499abbe6f9bd5d1a6cdeb2a4841a235d3f
Trump7 <ma332651@ucf.edu>
2024-08-22 11:40:55 -0400

    Finalized Header

--- time-frontend/src/components/Header.jsx
+++ time-frontend/src/components/Header.jsx
@@ -3,19 +3,26 @@ import Cookies from 'js-cookie';
 
 
 const Header = ({ isLoggedIn, userName, prevLogin }) => {
-  const prevLoginDate = prevLogin.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
-  const prevLoginTime = prevLogin.toLocaleTimeString('en-US');
+  let prevLoginDate = '';
+  let prevLoginTime = '';
+  
+  if(isLoggedIn){
+    const prevLoginObj = new Date(prevLogin);
+    prevLoginDate = prevLoginObj.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
+    prevLoginTime = prevLoginObj.toLocaleTimeString('en-US');
+  }
+  
 
   return (
     <header className="bg-main-background p-4 text-black flex justify-between items-center">
       <h1 className="text-5xl">Time Guard</h1>
       {isLoggedIn ? (
         <>
-        <div className="absolute left-1/2 transform -translate-x-1/2 text-md">
-          <span className="text-md mr-4">Last Login: {prevLoginDate} {prevLoginTime}</span>
+        <div className="flex-grow text-center">
+          <span className="text-md">Last Login: {prevLoginDate} {prevLoginTime}</span>
         </div>
-        <div className="flex items-center">
-          <span className="text-xl mr-4">Welcome Buffkin Tile {userName}</span>
+        <div className="flex items-center ml-auto">
+          <span className="text-xl mr-4">Welcome {userName}</span>
           <button
             onClick={onLogout}
             className="bg-button-color text-black text-xl px-3 py-2 rounded-3xl hover:bg-button-hover shadow-md"
