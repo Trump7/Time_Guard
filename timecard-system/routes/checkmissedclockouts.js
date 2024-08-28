031d2e368887a707f7fddf0cd19c93737c5873c4
Trump7 <ma332651@ucf.edu>
2024-08-28 16:23:42 -0400

    Completed CheckMissedClockouts.js
    Made this method such that if a user forgets to clock out, their hours will not be added to their total hours

--- timecard-system/routes/checkmissedclockouts.js
+++ timecard-system/routes/checkmissedclockouts.js
@@ -5,11 +5,28 @@ const { verifyDeviceToken } = require('../middleware/authMiddleware');
 
 //Function for Arduino
 router.post('/', verifyDeviceToken, async(req, res) => {
-    const result = await Timecard.updateMany(
-        {clockOut: null, clockIn: { $lt: new Date().setHours(0,0,0,0)}},
-        {status: 'Did not clock out', clockOut: 'N/A'}
-    );
-    res.send(result);
+    try {
+        //getting timecards where there is no clockOut
+        const missedClockouts = await Timecard.find({ clockOut: null, clockIn: { $lt: new Date().setHours(0, 0, 0, 0) } });
+
+        if (missedClockouts.length > 0) {
+            for (let timecard of missedClockouts) {
+                //clockOut = clockIn so no hours are added
+                timecard.clockOut = timecard.clockIn;
+                timecard.status = 'Did not clock out';
+                timecard.note = 'Automatically set clock-out to clock-in due to missed clock-out. No hours added.';
+
+                await timecard.save();
+                console.log(`Updated timecard for user ${timecard.userId}: Set clockOut to clockIn due to missed clock-out.`);
+            }
+            res.send({ message: `${missedClockouts.length} timecards updated.` });
+        } else {
+            res.send({ message: 'No missed clock-outs found.' });
+        }
+    } catch (error) {
+        console.error('Error processing missed clock-outs:', error);
+        res.status(500).send({ message: 'Error processing missed clock-outs.', error });
+    }
 });
 
 module.exports = router;
