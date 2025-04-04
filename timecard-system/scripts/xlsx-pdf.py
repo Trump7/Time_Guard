b8d35aa770a76625409ea69491eb020db5e582b0
Trump7 <89662276+Trump7@users.noreply.github.com>
2025-04-04 14:07:15 -0400

    Adjusted Python Code
    Adjusted the xlsx-pdf.py file for formatting.
    First attempt showed it was not following excels formatting.
    This update brought over all formatting needed.

--- timecard-system/scripts/xlsx-pdf.py
+++ timecard-system/scripts/xlsx-pdf.py
@@ -7,9 +7,27 @@ def export(excel_path, pdf_path):
 
     try:
         workbook = excel.Workbooks.Open(excel_path)
+        ws = workbook.Worksheets(1)
+
+        #PageSetup
+        ws.PageSetup.Orientation = 2
+        ws.PageSetup.Zoom = 85
+        ws.PageSetup.PaperSize = 1
+        ws.PageSetup.PrintQuality = 300
+        ws.PageSetup.FirstPageNumber = 1
+        ws.PageSetup.TopMargin = excel.InchesToPoints(0.75)
+        ws.PageSetup.BottomMargin = excel.InchesToPoints(0.75)
+        ws.PageSetup.LeftMargin = excel.InchesToPoints(0.7)
+        ws.PageSetup.RightMargin = excel.InchesToPoints(0.7)
+        ws.PageSetup.HeaderMargin = excel.InchesToPoints(0.3)
+        ws.PageSetup.FooterMargin = excel.InchesToPoints(0.3)
+        ws.PageSetup.AlignMarginsHeaderFooter = True
+        ws.PageSetup.ScaleWithDocHeaderFooter = True
+        ws.PageSetup.PrintArea = "A1:K25"
+        ws.PageSetup.PrintTitleRows = "$5:$5"
+        ws.PageSetup.Order = 1
 
         workbook.ExportAsFixedFormat(0, pdf_path)
-
         print(f"Exported PDF to {pdf_path}")
     except Exception as e:
         print(f"Error exporting PDF: {e}")
